ed6636bb42fdf644ca2539e5a4f64164cd9ff6bd
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-26 17:45:25 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,5 @@ def introduction(name)
 end
 
 def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}.""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
